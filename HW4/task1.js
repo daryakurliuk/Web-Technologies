5e451f5c051d38b5b14c80d9ccb088e6cced9b8a
Darya Kurliuk <darya.kurliuk@LeverX.local>
2019-01-09 14:36:14 +0300

    Task4

--- HW4/task1.js
+++ HW4/task1.js
@@ -2,15 +2,14 @@ var newDiv = document.createElement("div");
 var name = prompt("Write your username", "Username");
 if (/[0-9]/.test(name)) {
     for(var i = 0; i<name.length; i++) {
-        if(i%2 == 0) {
+        if(name[i+1]) {
             console.log(name[i].toUpperCase());
             newDiv.innerHTML += name[i].toUpperCase();
-            if(name[i+1]) newDiv.innerHTML += name[i+1].toLowerCase();
-        }
+            }
     }
 }
 else {
-newDiv.innerHTML = name;
+    newDiv.innerHTML = name.split("").reverse().join("");
 }
 var container = document.getElementById("container");
 container.appendChild(newDiv);
