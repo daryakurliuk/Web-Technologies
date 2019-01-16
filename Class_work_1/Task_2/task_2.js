d88ce0357e4082748c91205edab1f92f7b860140
Darya Kurliuk <darya.kurliuk@LeverX.local>
2019-01-16 13:53:29 +0300

    commit

--- Class_work_1/Task_2/task_2.js
+++ Class_work_1/Task_2/task_2.js
@@ -1,7 +1,7 @@
 for (var i = 0; i < 10; i++) {
     var el = document.createElement("div");
     el.classList.add("list-item");
-    el.innerHTML = '<img src="img_2.jpg" class="list-img"><div class="name">Dasha</div>';
+    el.innerHTML = '<img src="../Task_1/img_1.jpg" class="list-img"><div class="name">Dasha</div>';
     var cont = document.getElementById("list");
     cont.appendChild(el);    
 }
@@ -9,7 +9,7 @@ for (var i = 0; i < 10; i++) {
 for (var i = 0; i < 20; i++) {
     var el = document.createElement("div");
     el.classList.add("message");
-    el.innerHTML = '<img src="img_2.jpg" class="message-img"><div class="message-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus repellendus iste facere libero itaque eveniet. Nemo laborum, maiores et accusamus inventore doloremque pariatur, quisquam laudantium explicabo libero dolores, consequatur repellendus.</div>';
+    el.innerHTML = '<img src="../Task_1/img_1.jpg" class="message-img"><div class="message-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus repellendus iste facere libero itaque eveniet. Nemo laborum, maiores et accusamus inventore doloremque pariatur, quisquam laudantium explicabo libero dolores, consequatur repellendus.</div>';
     var cont = document.getElementById("messages");
     cont.appendChild(el);    
 }
--- Task_1/leverx.js
+++
@@ -1,9 +0,0 @@
-var main = document.getElementById("content_id");
-var card = document.getElementsByClassName("card")[0];
-console.log(main);
-console.log(card);
-for (var i = 0; i < 11; i++) {
-        var element = card.cloneNode(true);
-        main.appendChild(element);
-}
-   
