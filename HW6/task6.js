141c624b13d48b72321faba5b36e9f7fef3f16d5
Darya Kurliuk <darya.kurliuk@LeverX.local>
2019-01-21 11:57:12 +0300

    commit

--- HW6/task6.js
+++ HW6/task6.js
@@ -3,19 +3,19 @@ function begin() {
 }
 
 function getRequest(method, url) {
-    var xhr = new XMLHttpRequest();
+    var getRequest = new XMLHttpRequest();
 
-    xhr.open(method, url);
-    xhr.send();
-    xhr.onload = function ()
+    getRequest.open(method, url);
+    getRequest.send();
+    getRequest.onload = function ()
     {
-    if (xhr.readyState == 4 && xhr.status == "200")
+    if (getRequest.readyState == 4 && getRequest.status == "200")
         {
-            var data = JSON.parse(xhr.responseText);
+            var data = JSON.parse(getRequest.responseText);
             printTable(data);  
         }
     }
-    xhr.onerror = function () {
+    getRequest.onerror = function () {
     alert('Ошибка ' + this.status);
     }
 }
@@ -33,76 +33,44 @@ function printTable(data) {
 }
 
 function CreateData() {
+
     var data = {};
-    data[inpUserID.name] = parseInt(inpUserID.value, 10);
+    data[userId.name] = parseInt(userId.value, 10);
     data[inpID.name] = parseInt(inpID.value, 10);
     data[inpTitle.name] = inpTitle.value;
     data[inpBody.name] = inpBody.value;
-
 
     var postRequest = new XMLHttpRequest();
     postRequest.open('POST', 'http://localhost:3000/posts');
     postRequest.setRequestHeader('Content-Type', 'application/json');
     postRequest.send(JSON.stringify(data));
-    console.log(data);
-    UpdateData();
-
 }
 
 function UpdateData() {
 
     var idToUpdate = document.getElementById('inpUpdID').value;
     var data = {};
+
     data[inpUpdUserID.name] = parseInt(inpUpdUserID.value, 10);
     data[inpUpdID.name] = parseInt(inpUpdID.value, 10);
     data[inpUpdTitle.name] = inpUpdTitle.value;
     data[inpUpdBody.name] = inpUpdBody.value;
 
     var putRequest = new XMLHttpRequest();
-    putRequest.open('PUT', 'http://localhost:3000/posts/1' + idToUpdate);
+    putRequest.open('PUT', 'http://localhost:3000/posts/' + idToUpdate);
     putRequest.setRequestHeader("Content-Type", "application/json");
     putRequest.send(JSON.stringify(data));
-    console.log(data);
 
-
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/posts', true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onload = function () {
-        var dataAdd = JSON.parse(request.responseText);
-        change(dataAdd);
-        console.log(dataAdd);
-    };
-
-    request.send();
 }
 
 function DeleteData() {
+
     var idToDelete = parseInt(document.getElementById('inpDel').value, 10);
-    var request = new XMLHttpRequest();
-    request.open('DELETE', 'http://localhost:3000/posts' + idToDelete);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send();
 
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/posts');
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onload = function () {
-        var data = JSON.parse(request.responseText);
-        change(data);
-        console.log(data);
-    };
+    var deleteRequest = new XMLHttpRequest();
+    deleteRequest.open("DELETE",'http://localhost:3000/posts/' + idToDelete, true);
+    deleteRequest.setRequestHeader('Content-type', 'application/json');
+    deleteRequest.send();
 
-}
-
-function change(data) {
-    var str = " "
-    for (var i = 0; i < data.length; i++) {
-        str += "<tr><td>" + data[i].userId + "</td>" +
-            "<td>" + data[i].id + "</td>" +
-            "<td>" + data[i].title + "</td>" +
-            "<td>" + data[i].body + "</td></tr>";
     }
 
-    document.getElementById("ttbody").innerHTML = str;
-}
